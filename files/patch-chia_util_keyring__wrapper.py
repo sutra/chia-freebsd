c7f6e2defb488d98aab4f831c99e4008b5962f43
Joe Zhou <zhoushuqun@gmail.com>
2022-03-17 11:46:04 +0000

    Upgrade from 1.2.11 to 1.3.0.

--- files/patch-chia_util_keyring__wrapper.py
+++ files/patch-chia_util_keyring__wrapper.py
@@ -1,4 +1,4 @@
---- chia/util/keyring_wrapper.py.orig	2021-11-10 02:27:33 UTC
+--- chia/util/keyring_wrapper.py.orig	2022-03-15 02:48:42 UTC
 +++ chia/util/keyring_wrapper.py
 @@ -34,9 +34,9 @@ def get_legacy_keyring_instance() -> Optional[LegacyKe
          return MacKeyring()
@@ -7,8 +7,8 @@
 -    elif platform == "linux":
 +    elif platform == "linux" or platform.startswith("freebsd"):
          keyring: CryptFileKeyring = CryptFileKeyring()
--        keyring.keyring_key = "your keyring password"  # type: ignore
-+        keyring.keyring_key = "your keyring password"  # type: ignore
+-        keyring.keyring_key = "your keyring password"
++        keyring.keyring_key = "your keyring password"
          return keyring
      return None
  
--- files/patch-setup.py
+++ files/patch-setup.py
@@ -1,16 +1,17 @@
---- setup.py.orig	2021-11-10 01:01:55 UTC
+--- setup.py.orig	2022-03-15 02:56:36 UTC
 +++ setup.py
-@@ -1,37 +1,37 @@
+@@ -1,43 +1,43 @@
  from setuptools import setup
  
  dependencies = [
 -    "multidict==5.1.0",  # Avoid 5.2.0 due to Avast
--    "blspy==1.0.6",  # Signature library
--    "chiavdf==1.0.3",  # timelord and vdf verification
--    "chiabip158==1.0",  # bip158-style wallet filters
--    "chiapos==1.0.6",  # proof of space
+-    "aiofiles==0.7.0",  # Async IO for files
+-    "blspy==1.0.9",  # Signature library
+-    "chiavdf==1.0.5",  # timelord and vdf verification
+-    "chiabip158==1.1",  # bip158-style wallet filters
+-    "chiapos==1.0.9",  # proof of space
 -    "clvm==0.9.7",
--    "clvm_rs==0.1.15",
+-    "clvm_rs==0.1.19",
 -    "clvm_tools==0.4.3",
 -    "aiohttp==3.7.4",  # HTTP server for full node rpc
 -    "aiosqlite==0.17.0",  # asyncio wrapper for sqlite, to store blocks
@@ -23,12 +24,13 @@
 -    "keyring==23.0.1",  # Store keys in MacOS Keychain, Windows Credential Locker
 -    "keyrings.cryptfile==1.3.4",  # Secure storage for keys on Linux (Will be replaced)
 +    "multidict>=5.1.0",  # Avoid 5.2.0 due to Avast
-+    "blspy>=1.0.6",  # Signature library
-+    "chiavdf>=1.0.3",  # timelord and vdf verification
-+    "chiabip158>=1.0",  # bip158-style wallet filters
-+    "chiapos>=1.0.6",  # proof of space
++    "aiofiles>=0.7.0",  # Async IO for files
++    "blspy>=1.0.9",  # Signature library
++    "chiavdf>=1.0.5",  # timelord and vdf verification
++    "chiabip158>=1.1",  # bip158-style wallet filters
++    "chiapos>=1.0.9",  # proof of space
 +    "clvm>=0.9.7",
-+    "clvm_rs>=0.1.15",
++    "clvm_rs>=0.1.19",
 +    "clvm_tools>=0.4.3",
 +    "aiohttp>=3.7.4",  # HTTP server for full node rpc
 +    "aiosqlite>=0.17.0",  # asyncio wrapper for sqlite, to store blocks
@@ -46,16 +48,25 @@
 -    "setproctitle==1.2.2",  # Gives the chia processes readable names
 -    "sortedcontainers==2.4.0",  # For maintaining sorted mempools
 -    "websockets==8.1.0",  # For use in wallet RPC and electron UI
--    "click==7.1.2",  # For the CLI
--    "dnspythonchia==2.2.0",  # Query DNS seeds
--    "watchdog==2.1.6",  # Filesystem event watching - watches keyring.yaml
 +    "PyYAML>=5.4.1",  # Used for config file format
 +    "setproctitle>=1.2.2",  # Gives the chia processes readable names
 +    "sortedcontainers>=2.4.0",  # For maintaining sorted mempools
 +    "websockets>=8.1.0",  # For use in wallet RPC and electron UI
+     # TODO: when moving to click 8 remove the pinning of black noted below
+-    "click==7.1.2",  # For the CLI
+-    "dnspythonchia==2.2.0",  # Query DNS seeds
+-    "watchdog==2.1.6",  # Filesystem event watching - watches keyring.yaml
+-    "dnslib==0.9.17",  # dns lib
+-    "typing-extensions==4.0.1",  # typing backports like Protocol and TypedDict
+-    "zstd==1.5.0.4",
+-    "packaging==21.0",
 +    "click>=7.1.2",  # For the CLI
-+    "dnspython>=2.1.0",  # Query DNS seeds
++    "dnspythonchia>=2.2.0",  # Query DNS seeds
 +    "watchdog>=2.1.6",  # Filesystem event watching - watches keyring.yaml
++    "dnslib>=0.9.17",  # dns lib
++    "typing-extensions>=4.0.1",  # typing backports like Protocol and TypedDict
++    "zstd>=1.5.0.4",
++    "packaging>=21.0",
  ]
  
  upnp_dependencies = [
